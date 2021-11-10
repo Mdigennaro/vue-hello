4c14b3eb387d9474baaeebe66fa2433dca55efc3
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-10 15:28:23 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -7,6 +7,21 @@ const app = new Vue({
   data: {
     nome: 'Michele',
     cognome: 'Di Gennaro',
+    squadra:''
+  },
+
+  methods: {
+    squadraInter(){
+      this.squadra = 'https://images2.gazzettaobjects.it/assets-mc/calcio/squadre/high/127.png'
+    },
+
+    squadraMilan(){
+      this.squadra = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Logo_of_AC_Milan.svg/1200px-Logo_of_AC_Milan.svg.png'
+    },
+
+    squadraJuventus(){
+      this.squadra = 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Juventus_FC_2017_icon_%28black%29.svg/1200px-Juventus_FC_2017_icon_%28black%29.svg.png'
+    }
   }
 
 });
